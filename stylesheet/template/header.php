699aca56a312194da6a952852943ecefb13ea12a
AriesKira <linharesrodrigues.gil@gmail.com>
2022-04-26 16:45:28 +0200

    update : finshed footer and updated  header
    Added :
    -White stylesheet
    -Header page icons
    
    fix : footer css

--- stylesheet/template/header.php
+++ stylesheet/template/header.php
@@ -14,7 +14,7 @@ require "functions.php";
 
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="stylesheet" href="/PA/stylesheet/css/style.css">
+	<link id="myCss" rel="stylesheet" href="/PA/stylesheet/css/styleDark.css">
 	<script src="/PA/authentication/authentification.js"></script>
 	<script src="https://kit.fontawesome.com/ae4e8edb66.js" crossorigin="anonymous"></script>
 	<title>aROOTS - Home</title>
@@ -24,7 +24,23 @@ require "functions.php";
 	<header>
 		<nav class="navbar navbar-expand-lg ">
 			<div class="container-fluid">
-				<a class="navbar-brand nav-link" alt="Page d'accueil" href="/PA/index.php">Accueil</a>
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item active">
+						<a class="nav-link" alt="Page d'accueil" href="/PA/index.php">Accueil</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" alt="Page articles" href="">Articles</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" alt="Page d'entrainement" href="">Entrainement</a>
+					</li>
+					<li class="nav-item active">
+						<a class="nav-link" alt="Page forum" href="">Forum</a>
+					</li>
+					<li>
+						<a class="nav-link" alt="Page conseil" href="">Nos conseils</a>
+					</li>
+				</ul>
 				<input id="searchbar" alt="Barre de recherche" type="text" name="search" placeholder="     ---Recherche---">
 				<button class="navbar-toggler" type="button" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
@@ -49,6 +65,7 @@ require "functions.php";
 						<a id="ModeButton" type="button" class="btn btn-outline-light">
 							<i id="modeSwitch" class="fa-solid fa-sun fa-2x"></i>
 						</a>
+					</ul>
 				</div>
 			</div>
 		</nav>
