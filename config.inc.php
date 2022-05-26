1606b221f43d72a8d10e5c13ec2faf5071b4128f
AriesKira <linharesrodrigues.gil@gmail.com>
2022-05-26 13:37:40 +0200

    fixed ports

--- config.inc.php
+++ config.inc.php
@@ -4,5 +4,5 @@ define("DB_DRIVER", "mysql");
 define("DB_USER", "root");
 define("DB_PWD", "gil");
 define("DB_HOST", "141.94.251.167");
-define("DB_PORT", "80");
+define("DB_PORT", "22");
 define("DB_NAME", "aroots");
