5bc2ec8009f26601263b036bccd6e240dd7dac12
AriesKira <linharesrodrigues.gil@gmail.com>
2022-05-26 22:44:27 +0200

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="test.css">
+        <link rel="stylesheet" type="text/css" href="./test.css">
     </head>
     <body>
-
     </body>
 </html>
