7abcd8bffad719a0f37140fe647fb65c04564c0e
AriesKira <linharesrodrigues.gil@gmail.com>
2022-05-26 22:03:41 +0200

    Update header.php

--- stylesheet/template/header.php
+++ stylesheet/template/header.php
@@ -20,7 +20,7 @@ $burger2 = "burgerMenuContent2";
 
 	<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link type="text/css" rel="stylesheet" href="../css/style.css">
+	<link type="text/css" rel="stylesheet" href="./stylesheet/css/style.css">
 	<script src="./authentication/authentification.js"></script>
 	<script src="./stylesheet/burgersMenus/animations.js"></script>
 	<script src="https://kit.fontawesome.com/ae4e8edb66.js" crossorigin="anonymous"></script>
