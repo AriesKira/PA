9782ddc00cad0f52da2ed5a44cfec412f8fcdd29
AriesKira <linharesrodrigues.gil@gmail.com>
2022-05-26 13:14:56 +0200

    fixed header and footer
    removed advise and some other things

--- stylesheet/template/header.php
+++ stylesheet/template/header.php
@@ -2,10 +2,12 @@
 
 session_start();
 require "functions.php";
-if(isConnected()) {
+if (isConnected()) {
 	$userID = $_SESSION['id'];
 }
 
+$burger1 = "burgerMenuContent1";
+$burger2 = "burgerMenuContent2";
 
 ?>
 <!doctype html>
@@ -20,7 +22,10 @@ if(isConnected()) {
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link id="myCss" rel="stylesheet" href="/PA/stylesheet/css/style.css">
 	<script src="/PA/authentication/authentification.js"></script>
+	<script src="/PA/stylesheet/burgersMenus/animations.js"></script>
 	<script src="https://kit.fontawesome.com/ae4e8edb66.js" crossorigin="anonymous"></script>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>aROOTS - Home</title>
 </head>
 
@@ -28,22 +33,23 @@ if(isConnected()) {
 	<header>
 		<div class="container-fluid">
 			<nav class="navbar navbar-expand-lg ">
+				<button class="pageBurgers" onclick="burgerPopUp1()">
+					<i class="fa-solid fa-bars"></i>
+				</button>
 				<div class="col-sm">
+					<!-- AFFICHER POUR PC--->
 					<ul class="navbar-nav mr-auto">
 						<li class="nav-item active">
 							<a class="nav-link" alt="Page d'accueil" href="/PA/index.php">Accueil</a>
 						</li>
 						<li class="nav-item active">
-							<a class="nav-link" alt="Page articles" href="">Articles</a>
+							<a class="nav-link" alt="Page articles" href="/PA/articles.php">Articles</a>
 						</li>
 						<li class="nav-item active">
 							<a class="nav-link" alt="Page d'entrainement" href="">Entrainement</a>
 						</li>
 						<li class="nav-item active">
 							<a class="nav-link" alt="Page forum" href="">Forum</a>
-						</li>
-						<li>
-							<a class="nav-link" alt="Page conseil" href="">Nos conseils</a>
 						</li>
 					</ul>
 				</div>
@@ -56,23 +62,27 @@ if(isConnected()) {
 
 						<?php if (isConnected()) { ?>
 							<li class="nav-item">
-								<a class="nav-link" href="/PA/authentication/logout.php">Se deconnecter</a>
+								<a class="nav-link logs-links" href="/PA/authentication/logout.php">Se deconnecter</a>
 							</li>
 
 						<?php } else { ?>
 							<li class="nav-item">
-								<a class="nav-link" id="registerButton" onclick="popUpRegister()">S'inscrire</a>
+								<a class="nav-link logs-links" id="registerButton" onclick="popUpRegister()">S'inscrire</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link"id="loginButton" onclick="popUpLogin()">Se connecter</a>
+								<a class="nav-link logs-link" id="loginButton" onclick="popUpLogin()">Se connecter</a>
 							</li>
 
 						<?php } ?>
-						<a id="ModeButton" type="button" class="btn btn-outline-light">
+						<a id="ModeButton" type="button" class="btn btn-outline-light nav-link">
 							<i id="modeSwitch" class="fa-solid fa-sun fa-2x"></i>
 						</a>
+
 					</ul>
 				</div>
+				<button class="pageBurgers" onclick="burgerPopUp2()">
+					<i class="fa-solid fa-bars"></i>
+				</button>
 			</nav>
 		</div>
 	</header>
