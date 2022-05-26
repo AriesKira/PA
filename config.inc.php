a7c4846f78419181968f52044c079d18a4a745c1
AriesKira <linharesrodrigues.gil@gmail.com>
2022-05-26 14:03:08 +0200

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,7 @@
 <?php
 
 define("DB_DRIVER", "mysql");
-define("DB_USER", "root");
+define("DB_USER", "gil");
 define("DB_PWD", "gil");
 define("DB_HOST", "141.94.251.167");
 define("DB_PORT", "3306");
