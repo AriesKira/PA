999e4b07334fa812910d24c4abca5a9a72c5d727
AriesKira <linharesrodrigues.gil@gmail.com>
2022-05-28 17:47:20 +0200

    Update login.php

--- authentication/login.php
+++ authentication/login.php
@@ -12,31 +12,23 @@
                     $queryPrepared = $pdo->prepare("SELECT * FROM AROOTS_USERS WHERE email=:email");
                     $queryPrepared->execute(["email" => $_POST['email']]);
                     $results = $queryPrepared->fetch();
-
                     if (!empty($results) && password_verify($_POST['pwd'], $results['pwd'])) {
-
-
                         $token = createToken();
                         updateToken($results["idUser"], $token);
                         //Insertion dans la session du token
                         $_SESSION['email'] = $_POST['email'];
                         $_SESSION['idUser'] = $results["idUser"];
                         $_SESSION['token'] = $token;
-                        header("location: /PA/index.php");
+                        header("location: ../index.php");
                     } else {
                         echo '<div class="alert alert-danger mt-4 pb-1 " role="alert"><h5 class="fw-bold">Identifiants Incorrects</h5></div>';
                     }
                 }
-
                 ?>
-
                 <form method="POST" action="">
                     <input type="email" class="form-control" name="email" placeholder="Votre email" required="required"><br>
-
                     <input type="password" class="form-control" name="pwd" placeholder="Votre mot de passe" required="required"><br>
-
                     <input type="submit"  class="btn btn-outline-light mb-4 submitButton" value="Se connecter">
-
                 </form>
             </div>
         </div>
