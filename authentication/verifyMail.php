c90032e7a44153f81faf6fae2a97f340b56766ee
AriesKira <linharesrodrigues.gil@gmail.com>
2022-05-30 22:02:17 +0200

    Update verifyMail.php

--- authentication/verifyMail.php
+++ authentication/verifyMail.php
@@ -8,7 +8,7 @@ echo $user;
 $queryPrepared = $pdo->prepare("SELECT mailKey FROM AROOTS_USERS where pseudo= :pseudo");
 $queryPrepared->execute(["pseudo" => $user]);
 $mailKey = $queryPrepared->fetch();
-echo $mailKey;
+echo $mailKey[0];
 
 if($mailKey == $_GET['key']) {
     echo '4';
