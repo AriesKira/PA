cfc84ee5495a3db0e64eaaca99f727ecd4f71b06
AriesKira <linharesrodrigues.gil@gmail.com>
2022-05-30 21:55:00 +0200

    Update verifyMail.php

--- authentication/verifyMail.php
+++ authentication/verifyMail.php
@@ -7,13 +7,13 @@ $pdo = connectDB();
 echo $user;
 $queryPrepared = $pdo->prepare("SELECT mailKey FROM AROOTS_USERS where pseudo= :pseudo");
 $queryPrepared->execute(["pseudo" => $user]);
-echo '3';
 $mailKey = $queryPrepared->fetch();
+echo '3';
 
 if($mailKey == $_GET['key']) {
     echo '4';
-    $queryPrepared = $pdo->prepare("UPDATE AROOTS_USERS SET validated = 1 where idUser= $user");
-    $queryPrepared->execute(["validated" => 1]);
+    $queryPrepared = $pdo->prepare("UPDATE AROOTS_USERS SET validated = 1 where pseudo= :pseudo");
+    $queryPrepared->execute(["pseudo" => $user]);
 
     header("location: ../index.php");
 }
