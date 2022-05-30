dc67186508c083ab2d1da78d4dc2f22a2743fdce
AriesKira <linharesrodrigues.gil@gmail.com>
2022-05-30 23:29:14 +0200

    fixed incrementation

--- articles.php
+++ articles.php
@@ -3,67 +3,66 @@
 
 
 <?php
-	if(isWebmaster($userID)) {
+if (isWebmaster($userID)) {
 ?>
 
-    <div class="container">
-        <div class="row">
-            <div class="d-flex justify-content-center">
-                <button class="btn btn-info" id="add-btn">
-                    <a href="addArticle.php">Ajouter un article</a>
-                </button>
-            </div>
-        </div>
-    </div>
+	<div class="container">
+		<div class="row">
+			<div class="d-flex justify-content-center">
+				<button class="btn btn-info" id="add-btn">
+					<a href="addArticle.php">Ajouter un article</a>
+				</button>
+			</div>
+		</div>
+	</div>
 
 <?php
 }
 ?>
 
 <?php
-    $pdo = connectDB();
+$pdo = connectDB();
 
-            $queryPrepared = $pdo->prepare("SELECT * FROM AROOTS_ARTICLES");
-            $queryPrepared->execute();
-            $results = $queryPrepared->fetchAll();
+$queryPrepared = $pdo->prepare("SELECT * FROM AROOTS_ARTICLES");
+$queryPrepared->execute();
+$results = $queryPrepared->fetchAll();
 ?>
 
 
 
 <table class="table" id="articles-table">
-			<thead>
-				<tr>
-					<th>Auteur</th>
-					<th>Titre</th>
-					<th>Texte</th>
-				</tr>
-			</thead>
+	<thead>
+		<tr>
+			<th>Auteur</th>
+			<th>Titre</th>
+			<th>Texte</th>
+		</tr>
+	</thead>
 
-			<tbody>
-				
-				<?php
-				foreach ($results as $aroots_articles) {
-					echo
-						'<div id="articles-nav">
+	<tbody>
+
+		<?php
+		foreach ($results as $aroots_articles) {
+			echo
+			'<div id="articles-nav">
 								<tr>
-									<td>'.$aroots_articles["author"].'</td>
-									<td>'.$aroots_articles["title"].'</td>
-									<td>'.$aroots_articles["theme"].'</td>
-									<td>'.$aroots_articles["texte"].'</td>
-									<td>'.$aroots_articles["picture"].'</td>
+									<td>' . $aroots_articles["author"] . '</td>
+									<td>' . $aroots_articles["title"] . '</td>
+									<td>' . $aroots_articles["theme"] . '</td>
+									<td>' . $aroots_articles["texte"] . '</td>
+									<td>' . $aroots_articles["picture"] . '</td>
 										<div class="btn-group">
 										</div>
 									</td>
 								</tr>
-						</div>'
-						;
-				}
-				?>
+						</div>';
+		}
+		?>
 
-									
 
-			</tbody>
-		</table>
+
+	</tbody>
+</table>
 
 
 
